50db86dd4cff320b6ff9136782335ba22bef4c95
Eunhak Lee <lee@enak.kr>
2024-06-14 01:57:52 +0900

    chore: remove warning

--- src/main/java/live/luya/starfarm/common/protocols/PacketRegistry.java
+++ src/main/java/live/luya/starfarm/common/protocols/PacketRegistry.java
@@ -36,16 +36,6 @@ public enum PacketRegistry {
     public static byte getPacketId(Class<? extends PacketFieldsDescriber> clazz) {
         for (PacketRegistry registry : values()) {
             if (registry.clazz.equals(clazz) || registry.clazz.isAssignableFrom(clazz)) {
-                byte id = getPacketIdInClass(clazz);
-
-                if (registry.packetId != id) {
-                    Logger.getLogger(String.valueOf(PacketRegistry.class)).warning("Packet Class %s has mismatched mapping packetId=%d (registry has id=%d)".formatted(
-                            clazz.getCanonicalName(),
-                            id,
-                            registry.packetId
-                    ));
-                }
-
                 return registry.packetId;
             }
         }
